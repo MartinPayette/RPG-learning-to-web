4fabface1fcc4f95e8139a944dc2212fc0381925
Jessy Pouliot <jessypouliot98@gmail.com>
2024-02-06 20:15:35 -0500

    wip

--- .scripts/serve.mjs
+++ .scripts/serve.mjs
@@ -1,5 +1,8 @@
 import path from "path";
 import express from "express";
+import * as fs from "fs";
+import livereload from "livereload";
+import connectLiveReload from "connect-livereload";
 
 const APP_PORT = 8000;
 
@@ -8,17 +11,35 @@ const APP_PORT = 8000;
  */
 function serve(directory) {
     const app = express();
-    app.use(express.static(directory));
-    app.listen(APP_PORT, () => {
+
+    const liveReloadServer = livereload.createServer();
+    liveReloadServer.watch(directory);
+    liveReloadServer.server.once("connection", () => {
+        setTimeout(() => {
+            liveReloadServer.refresh("/");
+        }, 100);
+    });
+    app.use(connectLiveReload())
+
+    app.use(express.static(directory, { cacheControl: false }));
+
+    return app.listen(APP_PORT, () => {
         console.log(`App started at http://localhost:${APP_PORT}/`)
         console.log(`Serving files from "${directory}"`)
-    })
+    });
 }
 
 function main() {
     const dir = process.argv[2];
     const pathToServe = path.resolve(process.cwd(), dir);
-    serve(pathToServe);
+    const watcher = fs.watch(
+        pathToServe,
+        {
+            persistent: true,
+            recursive: true,
+        }
+    );
+    serve(pathToServe, watcher);
 }
 
 main();
